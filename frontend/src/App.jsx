b6028e8b2b0e4f5162069adff1d555eeaebca2f8
Divyansh <officialdslc1552005@gmail.com>
2025-10-10 19:41:58 +0530

    Created 75 percent of home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
 import './index.css'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Doctors from './pages/Doctors.jsx'
+import Login from './pages/Login.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import MyProfile from './pages/MyProfile.jsx'
+import MyAppointments from './pages/MyAppointments.jsx'
+import Appointment from './pages/Appointment.jsx'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
  
 
   return (
-    <>
-      <h1 className="text-3xl font-bold text-indigo-700"> Cure Desk</h1>
-    </>
+    <div className="mx-4 sm:max-[10%]:">
+
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:speciality" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/myprofile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/appointment/:docId" element={<Appointment />} />
+
+      </Routes>
+      
+    </div>
   )
 }
 
