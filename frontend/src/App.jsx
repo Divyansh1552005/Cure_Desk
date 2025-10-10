4b9be1af233d8815f42c36be545fbfb25c7e7822
Divyansh <officialdslc1552005@gmail.com>
2025-10-10 16:42:24 +0530

    setup the folder

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-indigo-700"> Cure Desk</h1>
+    </>
+  )
+}
+
+export default App
