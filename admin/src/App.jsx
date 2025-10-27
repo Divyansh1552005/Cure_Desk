68ac99ba3295d50cc36d2b2ca2317504b5494178
Divyansh <officialdslc1552005@gmail.com>
2025-10-27 14:58:02 +0530

    Started admin panel

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -2,14 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './pages/Login.jsx'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
 
 
   return (
-    <>
-      <h1 className="text-3xl font-bold bg-amber-300">Welcome to Admin panel</h1>
-    </>
+    <div>
+      <Login />
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="colored"/>
+
+    </div>
   )
 }
 
